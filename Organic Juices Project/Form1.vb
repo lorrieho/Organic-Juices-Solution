4e6d8d232aaa43ca3fc772b504403a2491c918f7
lorrieho <lorrieho@yahoo.com>
2023-08-11 18:31:15 -0700

    Add project files.

---
+++ Organic Juices Project/Form1.vb
@@ -0,0 +1,18 @@
+﻿'organic juice app
+'Lorrie Ho
+'8.11.23
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Please insert a date and time")
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("Sorry, delivery unavailable")
+    End Sub
+End Class
